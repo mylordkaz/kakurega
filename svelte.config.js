576b72509276be0d61ff94c8b82064f40589da72
Kevin <128312170+mylordkaz@users.noreply.github.com>
2025-07-25 09:50:42 +0900

    add fix for favicon and variable for deploy

--- svelte.config.js
+++ svelte.config.js
@@ -5,12 +5,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			routes: {
-				include: ['/*'],
-				exclude: []
-			}
-		})
+		adapter: adapter(),
+		prerender: {
+			handleHttpError: 'warn'
+		}
 	}
 };
 
